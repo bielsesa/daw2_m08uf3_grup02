8e629c175fe2f4e9187742456a89bcb3a0c89212
bielsesa <bielsesa@gmail.com>
2020-03-05 11:03:49 +0100

    Creació d'usuaris

--- creacio-user.php
+++ creacio-user.php
@@ -4,45 +4,37 @@
             echo "No has iniciat sessió com a administrador.<br/>";
             echo "Torna a la <a href=\"http://localhost/index.html\">pàgina inicial</a> i fes login.";
         } else {
-            $uid = $_POST["uid"];
-            $ou = $_POST["ou"];
-            $nom = $_POST["nom"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
-            $uid = $_POST["uid"];
+            $ldaphost = "ldap://localhost";
+            $ldappass = $_SESSION["pwd"];
+            $ldapadmin = $_SESSION["id"];
 
-            // Creació de l'usuari LDAP
-            $createStatement = <<<STM
-            dn: uid=$uid,ou=$ou,dc=fjeclot,dc=net
-            objectClass: top
-            objectClass: person
-            objectClass: organizationalPerson
-            objectClass: inetOrgPerson
-            objectClass: posixAccount
-            objectClass: shadowAccount
-            uid: $uid
-            cn: $nom $cognom
-            sn: sodalloc
-            givenName: leinad
-            title: administrador de sistemes
-            telephoneNumber: 93 456 34 23
-            mobile: 657 89 89 89
-            postalAddress: Carrer Valencia 27. Barcelona.
-            loginShell: /bin/bash
-            gidNumber: 2000
-            uidNumber: 1000
-            homeDirectory: /users/sysadmin/
-            description: Administrador de sistemes.
-            STM;
+            // Connexió LDAP
+            $ldapconn = ldap_connect("localhost") or die("No s'ha pogut establir una connexió amb el servidor openLDAP.");
 
+            if ($ldapconn) {
+                $ldapbind = ldap_bind($ldapconn, $ldapadmin, $ldappass);
+                
+                // Preparació de les dades
+                $info["uid"] = $_POST["uid"];
+                $info["ou"] = $_POST["ou"];
+                $info["nom"] = $_POST["nom"];
+                $info["cognom"] = $_POST["cognom"];
+                $info["titol"] = $_POST["titol"];
+                $info["telFix"] = $_POST["telFix"];
+                $info["telMob"] = $_POST["telMob"];
+                $info["adrPostal"] = $_POST["adrPostal"];
+                $info["loginShell"] = $_POST["loginShell"];
+                $info["gidNumber"] = $_POST["gidNumber"];
+                $info["uidNumber"] = $_POST["uidNumber"];
+                $info["homeDirectory"] = $_POST["homeDirectory"];
+                $info["desc"] = $_POST["desc"];
+                $info["pwd"] = $_POST["pwd"];
+
+                $usuari = "uid=".$info["uid"].",cn=".$info["nom"]." ".$info["cognom"].",ou=".$info["ou"].",dc=fjeclot,dc=net";
+
+                // Creació de l'usuari LDAP
+                $res = ldap_add($ldapconn, $usuari, $info);
+            }
         }
     }
 ?>
